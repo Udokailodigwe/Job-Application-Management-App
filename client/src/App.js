79482b5d5ad93609b2082c773ec1938409c39ac8
Udokailodigwe <ilodigweudoka@gmail.com>
2023-07-11 18:49:38 +0300

    Created landing page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
+import Landing from "./pages/Landing.js";
 function App() {
   return (
     <div>
       <h1>Jobify</h1>
+      <Landing />
     </div>
   );
 }
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,28 @@
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+
+const Landing = () => {
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Job Tacking application, to enable you keep track of your job
+            application, upcoming interviews and essentially organizing your job
+            search activities. Makes your life as a potential job candidate
+            stress free, so you can concentrate on landing your preferred job.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main>
+  );
+};
+export default Landing;
