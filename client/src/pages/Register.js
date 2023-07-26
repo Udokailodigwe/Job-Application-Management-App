3bfc72d1f70766b4f738f2ca7870f0723b66c5a2
Udokailodigwe <ilodigweudoka@gmail.com>
2023-07-26 23:17:59 +0300

    imported useState and useEffect hook

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 
 const Register = () => {
+  const [value, setValue] = useState([]);
+
+  useEffect(() => {}, []);
   return (
     <div>
       <h1>Register page</h1>
