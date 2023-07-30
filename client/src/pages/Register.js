c49a1c8a60ce35ee7ff8d1d95a13a6ea60a55535
Udokailodigwe <ilodigweudoka@gmail.com>
2023-07-30 23:58:45 +0300

    created initial state for form values

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,13 +1,29 @@
 import { useState, useEffect } from "react";
+import { logo } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 
 const Register = () => {
-  const [value, setValue] = useState([]);
+  const [value, setValue] = useState(initialState);
 
-  useEffect(() => {}, []);
+  const handleChange = (e) => {
+    console.log(e);
+  };
+
+  const onSubmit = (e) => {
+    console.log(e);
+  };
+
   return (
-    <div>
+    <Wrapper className="full-page">
       <h1>Register page</h1>
-    </div>
+    </Wrapper>
   );
 };
 
