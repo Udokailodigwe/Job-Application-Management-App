caf7a5f293cd4841d3699ea3cd0ac30c03296080
Udokailodigwe <ilodigweudoka@gmail.com>
2024-03-17 21:25:18 +0200

    Init

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,3 +1,9 @@
+import { useState } from "react";
+import AreaChart from "./AreaChart";
+import BarChart from "./BarChart";
+import { useSelector } from "react-redux";
+import Wrapper from "../assets/wrappers/ChartsContainer";
+
 const ChartsContainer = () => {
   return <h2>ChartsContainer</h2>;
 };
